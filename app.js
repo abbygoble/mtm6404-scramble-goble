ace041c7ea841f03e5be2f7cdad40a2c247145b2
Abby Goble <gobl0009@algonquinlive.com>
2024-07-08 19:21:03 -0400

    js is linked now

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+console.log('Hello World')
+
 /**********************************************
  * STARTER CODE
  **********************************************/
@@ -30,45 +32,27 @@
     return copy
   }
   
+  const fruit = 'banana'
+const fruits = ['strawberries', 'apples', 'bananas']
+
+console.log(shuffle(fruit))
+console.log(shuffle(fruits))
+
   /**********************************************
  * YOUR CODE BELOW
  **********************************************/
 
-  const words = {
-    arrowroot,
-    barbell,
-    crocodile,
-    destination,
-    elephant,
-    firetruck,
-    goose,
-    helicopter,
-    intelligent,
-    jail
-  }
+//   const words = [
+//     'arrowroot', 
+//     'barbell',
+//     'crocodile',
+//     'destination',
+//     'elephant',
+//     'firetruck',
+//     'goose',
+//     'helicopter',
+//     'intelligent',
+//     'jail'
+// ]
 
-  function shuffle (src) {
-    const copy = [...src]
-  
-    const length = copy.length
-    for (let i = 0; i < length; i++) {
-      const x = copy[i]
-      const y = Math.floor(Math.random() * length)
-      const z = copy[y]
-      copy[i] = z
-      copy[y] = x
-    }
-  
-    if (typeof src === 'string') {
-      return copy.join('')
-    }
-  
-    return copy
-  }
-  
-  
-  const fruit = 'banana'
-  const fruits = ['strawberries', 'apples', 'bananas']
-  
-  console.log(shuffle(fruit))
-  console.log(shuffle(fruits))
+//   ReactDOM.render(document.getElementById('root'))
